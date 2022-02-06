100e1e926091bdf993ba2591cb8af9deffa797e3
Simon LEONARD <git-1001af4@sinux.sh>
2022-02-06 23:18:16 +0100

    doc: ajout de typedoc

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,19 @@
 import express from 'express';
 
+/**
+ * Application express gérant les appelles aux fonctions en fonction du chemin
+ */
 const app = express();
 
-// Disable header mentioning Express
+// Désactive le header indiquant que c'est une application express
 app.disable('x-powered-by');
 
-// Handle API's root
 app.get('/', (req, res) => {
   // Return JSON with status 200
   res.status(200).send({ message: 'Hello World !' });
 });
 
+/**
+ * Application hydraté avec tout les middlewares
+ */
 export default app;
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import app from '@app';
 
+/** Port par défaut */
 const port = 3000;
 app.listen(port, () => {
   console.log('Server is running on port ' + port);
