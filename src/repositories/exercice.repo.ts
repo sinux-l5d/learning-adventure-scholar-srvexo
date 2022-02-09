b62f201e2a38955b140eb925a910a56e3a08b7c7
Simon LEONARD <git-1001af4@sinux.sh>
2022-02-09 16:09:39 +0100

    feat: get exo complet depuis la db (repo)

---
+++ src/repositories/exercice.repo.ts
@@ -0,0 +1,15 @@
+import { Exercice } from '@db/exercice.db';
+import { ExerciceComplet } from '@type/exercice/ExerciceComplet';
+
+/**
+ * @todo filtrer les attributs de l'exercice qui ne servent pas (uniquement côté id)
+ */
+export const getExerciceCompletById = async (
+  id: ExerciceComplet['id'],
+): Promise<ExerciceComplet> => {
+  const exo = await Exercice.findById(id).exec();
+  if (exo) {
+    return exo;
+  }
+  throw new Error('Not found'); // @todo : handle
+};
