fbc022bbb1f5418dcc35b8f079c32e9de2507b22
Simon LEONARD <git-1001af4@sinux.sh>
2022-02-09 16:10:50 +0100

    doc: sauvegarde todo

--- src/repositories/exercice.repo.ts
+++ src/repositories/exercice.repo.ts
@@ -2,7 +2,7 @@ import { Exercice } from '@db/exercice.db';
 import { ExerciceComplet } from '@type/exercice/ExerciceComplet';
 
 /**
- * @todo filtrer les attributs de l'exercice qui ne servent pas (uniquement côté id)
+ * @todo filtrer les attributs de l'exercice qui ne servent pas (attributs spécifiques à BDD) (Document & Exo & _id -> Exo)
  */
 export const getExerciceCompletById = async (
   id: ExerciceComplet['id'],
