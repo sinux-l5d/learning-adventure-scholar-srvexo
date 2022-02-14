9a8fc7672aeda95cbb166a1c4d477457d00eacba
Romain Pajean <pajeanromain@gmail.com>
2022-02-14 12:58:25 +0100

    feat: appel au service stratégie

--- src/routes/exercice.route.ts
+++ src/routes/exercice.route.ts
@@ -1,5 +1,6 @@
 import { Router, RequestHandler } from 'express';
 import { ExerciceService } from 'src/services/exercice.service';
+import { StrategieService } from 'src/services/strategie.service';
 
 const exerciceRouter = Router();
 
@@ -15,6 +16,16 @@ const getExerciceCompletById: RequestHandler = async (req, res) => {
   res.status(200).json({ exercice: exo });
 };
 
+const getExerciceCompletNext: RequestHandler = async (req, res) => {
+  const id = req.params.id;
+  // Demander au service sratégie l'id de l'exercice suivant
+  // Pour le moment pas de service stratégie donc codé en dur
+  const idSuivant = await StrategieService.callStrategieForNextId(id);
+  const exoSuivant = await ExerciceService.getExerciceCompletById(idSuivant);
+  res.status(200).json({ exercice: exoSuivant });
+};
+
 exerciceRouter.get('/:id', getExerciceCompletById);
+exerciceRouter.get('/:id/next', getExerciceCompletNext);
 
 export default exerciceRouter;
